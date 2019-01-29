741f1e3dbeaa3c6e300856fbea03f09221ac4f95
Marcus Wilson <mswilson4040@hotmail.com>
2019-01-28 16:12:39 -0800

    changed text

--- src/stringserver.js
+++ src/stringserver.js
@@ -1,8 +1,9 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
+const http = require('http');
+const server = http.createServer( (req, res) => {
     res.writeHead(200);
-    res.write("Hello World");
+    res.write(`Hello World, the current time is ${new Date()}`);
     res.end();
 });
-server.listen(process.env.PORT || 8080);
-console.log("Listening on http://127.0.0.1:8080/");
+const port = process.env.PORT || 8080;
+server.listen(port);
+console.log(`Listening on http://127.0.0.1:${port}/`);
